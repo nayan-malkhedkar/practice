a19e524bf560149687ca93755e5ec4765748141a
nayan-malkhedkar <nayan.malkhedkar@gmail.com>
2025-09-29 16:19:47 +0530

    Added adidas excel data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.functions.execute import execute_query
 
 # Read excel data
 df = pd.read_csv('utils/data/employees.csv')
+df1 = pd.read_csv('utils/data/adidas.csv')
 
 # Filter based on first rows or headers of Excel sheet
 employeeRole = df.filter(items=['Name','Designation'])
@@ -11,7 +12,10 @@ employeeRole = df.filter(items=['Name','Designation'])
 # Filter based on values of cells and display rows
 employeeNoOffice = df[df['Office'].isna()]
 employeeEngineer = df[df['Designation'].str.contains('Engineer')]
+shoeName = df.filter(items=['Name'])
+print(employeeNoOffice)
 print(employeeEngineer)
+print(shoeName)
 
 # Pull data from database
 all_data = execute_query(fetchAll)
